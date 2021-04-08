84e80a4fc6c347d2f458aacea2b07f053701119c
Dang Tuan <dangminhtuan.arwen@gmail.com>
2021-04-08 15:46:17 +0700

    [Bài tập] Access modifier

--- Circle/Circle.java
+++ Circle/Circle.java
@@ -1,2 +1,20 @@
-package accessModifier.Circle;public class Circle {
+package accessModifier.Circle;
+
+public class Circle {
+    private double radius=1.0;
+    private String color = "red";
+
+    public Circle(){
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double getArea(double radius){
+        return Math.PI*radius*radius;
+    }
+
+    public double getRadius(){
+        return this.radius;
+    }
 }
---
+++ Circle/testCircle.java
@@ -0,0 +1,10 @@
+package accessModifier.Circle;
+
+public class testCircle {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(8);
+
+        System.out.println(circle1.getArea(5));
+        System.out.println( circle1.getRadius());
+    }
+}
