4b56fd56604b564f1e02c50826b74de994e321a0
Dang Tuan <dangminhtuan.arwen@gmail.com>
2021-04-08 14:21:21 +0700

    [Thực hành] Static Property

---
+++ TestStaticProperty/Cars.java
@@ -0,0 +1,30 @@
+package TestStaticProperty;
+
+public class Cars {
+    private String name;
+    private String engine;
+
+    public static int numberOfCars;
+
+    public Cars(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCars++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+}
---
+++ TestStaticProperty/TestStaticProperty.java
@@ -0,0 +1,10 @@
+package TestStaticProperty;
+
+public class TestStaticProperty {
+    public static void main(String[] args) {
+        Cars car1=new Cars("Mercedes", "V12");
+        System.out.println(Cars.numberOfCars);
+        Cars car2=new Cars("Bmw","V10");
+        System.out.println(Cars.numberOfCars);
+    }
+}
